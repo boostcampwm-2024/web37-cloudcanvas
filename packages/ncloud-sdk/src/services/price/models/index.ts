3c04b22e664c8ae68ea0f48087490aa8970020b2
Geonhyuk Seo <sgh4123@naver.com>
2024-11-27 18:40:04 +0900

    🚚 Setting(ncloud-sdk): price 관련 파라미터 인터페이스를 배럴 파일에 추가
    ISSUES CLOSED: #140

--- packages/ncloud-sdk/src/services/price/models/index.ts
+++ packages/ncloud-sdk/src/services/price/models/index.ts
@@ -1,2 +1,6 @@
 export * from './CommonCode';
 export * from './NcloudResponse';
+export * from './GetPriceRequest';
+export * from './GetProductCategoryListRequest';
+export * from './GetProductListRequest';
+export * from './GetProductPriceListRequest';
