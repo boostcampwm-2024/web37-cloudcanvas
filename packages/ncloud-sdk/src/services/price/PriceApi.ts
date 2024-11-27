e895d9ef1065cab9735780a26e66c2735b29704e
Geonhyuk Seo <sgh4123@naver.com>
2024-11-27 18:49:27 +0900

    ✨ Feat(ncloud-sdk): price sdk api 호출 기능 구현 완료
    ISSUES CLOSED: #140

--- packages/ncloud-sdk/src/services/price/PriceApi.ts
+++ packages/ncloud-sdk/src/services/price/PriceApi.ts
@@ -1,3 +1,9 @@
+import {
+    GetPriceListRequest,
+    GetProductCategoryListRequest,
+    GetProductListRequest,
+    GetProductPriceListRequest,
+} from './models';
 import { PriceApiClient } from './PriceApiClient';
 import { ApiKeyCredentials } from './types';
 /**
@@ -9,5 +15,41 @@ export class PriceApi {
     constructor(apiKey?: ApiKeyCredentials) {
         this.resourcePath = '/billing/v1';
         this.client = new PriceApiClient(apiKey);
+    }
+
+    async getPriceList(getPriceRequest: GetPriceListRequest) {
+        return await this.client.request({
+            method: 'POST',
+            url: this.resourcePath + '/product/getPriceList',
+            params: getPriceRequest,
+        });
+    }
+
+    async getProductCategoryList(
+        getProductCategoryListRequest: GetProductCategoryListRequest,
+    ) {
+        return await this.client.request({
+            method: 'POST',
+            url: this.resourcePath + '/product/getProductCategoryList',
+            params: getProductCategoryListRequest,
+        });
+    }
+
+    async getProductList(getProductListRequest: GetProductListRequest) {
+        return await this.client.request({
+            method: 'POST',
+            url: this.resourcePath + '/product/getProductList',
+            params: getProductListRequest,
+        });
+    }
+
+    async getProductPriceList(
+        getProductPriceListRequest: GetProductPriceListRequest,
+    ) {
+        return await this.client.request({
+            method: 'POST',
+            url: this.resourcePath + '/product/getProductPriceList',
+            params: getProductPriceListRequest,
+        });
     }
 }
--- packages/ncloud-sdk/src/services/price/models/index.ts
+++ packages/ncloud-sdk/src/services/price/models/index.ts
@@ -1,6 +1,6 @@
 export * from './CommonCode';
 export * from './NcloudResponse';
-export * from './GetPriceRequest';
+export * from './GetPriceListRequest';
 export * from './GetProductCategoryListRequest';
 export * from './GetProductListRequest';
 export * from './GetProductPriceListRequest';
