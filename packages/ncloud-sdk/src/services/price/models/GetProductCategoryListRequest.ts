f9a0b4d2a545ded6ec6f048c0bf896b264912631
Geonhyuk Seo <sgh4123@naver.com>
2024-11-27 18:37:18 +0900

    ✨ Feat(ncloud-sdk): price 관련 SDK API를 호출하기 위한 파라미터 인터페이스 구현
    ISSUES CLOSED: #140

---
+++ packages/ncloud-sdk/src/services/price/models/GetProductCategoryListRequest.ts
@@ -0,0 +1,10 @@
+export interface GetProductCategoryListRequest {
+    /**
+     * 상품 카테고리 코드
+     */
+    productCategoryCode?: string;
+    /**
+     * 응답 결과의 형식
+     */
+    responseFormatType?: string;
+}
