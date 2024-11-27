a2182c46cd0e0a35bde9665903b4c6c02611afd7
Geonhyuk Seo <sgh4123@naver.com>
2024-11-28 00:27:29 +0900

    🚚 Setting(server): ncloud api 호출 테스트 완료
    ISSUES CLOSED: #140

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -5,13 +5,15 @@ import { ValidationPipe } from '@nestjs/common';
 import helmet from 'helmet';
 import cookieParser from 'cookie-parser';
 import { PrismaExceptionFilter } from './filters/prisma-exception.filter.js';
-import { Ncloud, PriceApiClient, PriceApi } from '@cloud-canvas/ncloud-sdk';
+import { ApiKeyCredentials, Ncloud, PriceApi } from '@cloud-canvas/ncloud-sdk';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     const ncloud = new Ncloud();
-    console.log(ncloud.keys());
-
+    const priceApi = new PriceApi(ncloud.keys() as ApiKeyCredentials);
+    console.log(priceApi);
+    const result = await priceApi.getProductCategoryList({});
+    console.log(result.getProductCategoryListResponse.productCategoryList);
     swaggerConfig(app);
 
     app.use(helmet());
