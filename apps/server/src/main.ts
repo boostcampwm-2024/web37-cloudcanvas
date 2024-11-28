139a8a95b5cfaffdc85d50c1feed334331f2a0dd
paulcjy <paulcjy@naver.com>
2024-11-28 12:55:45 +0900

    🚚 Setting(server): 개발 환경에서 enable cors 설정 추가

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -5,6 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 import helmet from 'helmet';
 import * as cookieParser from 'cookie-parser';
 import { PrismaExceptionFilter } from './filters/prisma-exception.filter';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -28,6 +29,25 @@ async function bootstrap() {
     const { httpAdapter } = app.get(HttpAdapterHost);
     app.useGlobalFilters(new PrismaExceptionFilter(httpAdapter));
 
+    const configService = app.get(ConfigService);
+    const environment = configService.get('NODE_ENV');
+
+    if (environment !== 'production') {
+        app.enableCors({
+            origin: 'http://localhost:3001',
+            methods: [
+                'GET',
+                'HEAD',
+                'PUT',
+                'PATCH',
+                'POST',
+                'DELETE',
+                'OPTIONS',
+            ],
+            allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
+            credentials: true,
+        });
+    }
     await app.listen(3000);
 }
 bootstrap();
