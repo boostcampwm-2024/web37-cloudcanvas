f31c17da903f86d87e90bbb0239f2273ff59f639
paulcjy <paulcjy@naver.com>
2024-11-28 12:31:27 +0900

    🐞 Fix(server): user 데코레이터에서 유저 객체가 없는 경우도 에러 없이 작동하도록 변경

--- apps/server/src/decorators/user.decorator.ts
+++ apps/server/src/decorators/user.decorator.ts
@@ -1,9 +1,4 @@
-import {
-    createParamDecorator,
-    ExecutionContext,
-    InternalServerErrorException,
-    UnauthorizedException,
-} from '@nestjs/common';
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { User as UserEntity } from '@prisma/client';
 
 export const User = createParamDecorator(
@@ -11,14 +6,6 @@ export const User = createParamDecorator(
         const request = ctx.switchToHttp().getRequest();
         const user = request.user;
 
-        if (!user) {
-            throw new UnauthorizedException('사용자 정보가 없습니다.');
-        }
-
-        if (!data) return user;
-        if (data in user) return user[data];
-        throw new InternalServerErrorException(
-            `사용자 정보에 ${data}이/가 없습니다.`,
-        );
+        return data ? user?.[data] : user;
     },
 );
