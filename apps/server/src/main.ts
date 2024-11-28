cad29ece3c271a742c15cca0cc6cd67aa42a6f7d
Geonhyuk Seo <sgh4123@naver.com>
2024-11-28 18:02:53 +0900

    ✨ Feat(server): ncloud server 리소스 가격 조회 배치 기능 구현 및 테스트 완료
    ISSUES CLOSED: #140

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -6,66 +6,9 @@ import helmet from 'helmet';
 import * as cookieParser from 'cookie-parser';
 import { PrismaExceptionFilter } from './filters/prisma-exception.filter';
 import { ConfigService } from '@nestjs/config';
-import { ApiKeyCredentials, Ncloud, PriceApi } from '@cloud-canvas/ncloud-sdk';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    // const ncloud = new Ncloud();
-    // const priceApi = new PriceApi(ncloud.keys() as ApiKeyCredentials);
-    // const result1 = await priceApi.getProductCategoryList({});
-    // console.log(result1.productCategoryList);
-
-    // VPC
-    // const result = await priceApi.getProductList({
-    //     regionCode: 'KR',
-    //     productCategoryCode: 'NETWORKING'
-    // });
-    // result.productList.forEach((product) => {
-    //     if(product.productCode === 'VPC.V001' || product.productCode === 'SPNET00000000015')
-    //         console.log(product);
-    // })
-
-    // SERVER
-    // const result = await priceApi.getProductList({
-    //     regionCode: 'KR',
-    //     productCategoryCode: 'COMPUTE',
-    // });
-    // result.productList.forEach((product) => {
-    //     if(product.productItemKind.codeName === 'Server' && product.serverProductCode){
-    //         console.log(product);
-    //     }
-    // })
-
-    // SERVER PRICE
-    // const result = await priceApi.getProductPriceList({
-    //     regionCode: 'KR',
-    //     payCurrencyCode: 'KRW',
-    //     productCategoryCode: 'COMPUTE',
-    // });
-    // const map: Map<string, Record<string, number | string>[]> = new Map();
-    // result.productPriceList.forEach((product) => {
-    //     if (
-    //         product.productItemKind.code === 'SVR' &&
-    //         product.serverProductCode &&
-    //         product.serverProductCode.endsWith('50')
-    //     ) {
-    //         if (!map.has(product.productType.codeName))
-    //             map.set(product.productType.codeName, []);
-    //         const {
-    //             serverProductCode,
-    //             priceList: [{ price: monthPrice }, { price: hourPrice }],
-    //         }: {
-    //             serverProductCode: string;
-    //             priceList: { price: number }[];
-    //         } = product;
-    //         map.get(product.productType.codeName).push({
-    //             serverProductCode: serverProductCode.toLowerCase(),
-    //             monthPrice,
-    //             hourPrice,
-    //         });
-    //     }
-    // });
-    // console.log(map);
 
     swaggerConfig(app);
 
