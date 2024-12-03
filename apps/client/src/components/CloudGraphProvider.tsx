911198df3c0552532a3bfaa4e8b60fd47394bfec
p1n9d3v <p1n9@p1n9s-MacBook-Air.local>
2024-12-03 17:37:08 +0900

    ✨ Feat(client): 초기 렌더링시 archiecture의 위치에 맞게 viewbox 조정 로직 추가

--- apps/client/src/components/CloudGraphProvider.tsx
+++ apps/client/src/components/CloudGraphProvider.tsx
@@ -5,6 +5,7 @@ import { GroupProvider } from '@contexts/GroupContext';
 import { NodeProvider } from '@contexts/NodeContext';
 import { SelectionProvider } from '@contexts/SelectionContext';
 import { SvgProvider } from '@contexts/SvgContext';
+import { calcViewBoxBounds } from '@helpers/viewBox';
 import { Edge, Group, Node } from '@types';
 import { ReactNode } from 'react';
 
@@ -20,7 +21,27 @@ export default ({ children, initialData }: Props) => {
     return (
         <DimensionProvider>
             <SvgProvider>
-                <GraphProvider initialZoomFactor={3}>
+                <GraphProvider
+                    initialViewBox={
+                        initialData?.nodes
+                            ? calcViewBoxBounds(
+                                  initialData?.nodes,
+                                  {
+                                      x: 0,
+                                      y: 0,
+                                      width: document.body.clientWidth * 3,
+                                      height: document.body.clientHeight * 3,
+                                  },
+                                  '2d',
+                              )
+                            : {
+                                  x: 0,
+                                  y: 0,
+                                  width: document.body.clientWidth * 2,
+                                  height: document.body.clientHeight * 2,
+                              }
+                    }
+                >
                     <GroupProvider initialGroups={initialData?.groups}>
                         <NodeProvider initialNodes={initialData?.nodes}>
                             <EdgeProvider initialEdges={initialData?.edges}>
---
+++ apps/client/src/helpers/viewBox.ts
@@ -0,0 +1,41 @@
+import { GRID_2D_SIZE } from '@constants';
+import { Dimension, Node, ViewBox } from '@types';
+import { calculateNodeBoundingBox } from './node';
+
+export const calcViewBoxBounds = (
+    nodes: Record<string, Node>,
+    viewBox: ViewBox,
+    dimension: Dimension,
+) => {
+    const allNodeBounds = calculateNodeBoundingBox(nodes, dimension);
+    const viewBoxCenter = {
+        x: viewBox.x + viewBox.width / 2,
+        y: viewBox.y + viewBox.height / 2,
+    };
+    const allNodeCenter = {
+        x: allNodeBounds.minX + allNodeBounds.width / 2,
+        y: allNodeBounds.minY + allNodeBounds.height / 2,
+    };
+
+    const diff = {
+        x: viewBoxCenter.x - allNodeCenter.x,
+        y: viewBoxCenter.y - allNodeCenter.y,
+    };
+
+    const padding = GRID_2D_SIZE * 8;
+    let newWidth =
+        viewBox.width < allNodeBounds.width
+            ? allNodeBounds.width + padding
+            : viewBox.width;
+    let newHeight =
+        viewBox.height < allNodeBounds.height
+            ? allNodeBounds.height + padding
+            : viewBox.height;
+
+    return {
+        x: viewBox.x - diff.x,
+        y: viewBox.y - diff.y,
+        width: newWidth,
+        height: newHeight,
+    };
+};
