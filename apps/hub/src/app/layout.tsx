b2a83b56cc9429a53e2043501bc6c7bf1ca9f983
paulcjy <paulcjy@naver.com>
2024-12-03 16:46:26 +0900

    ✨ Feat(hub): 로고 폰트 추가 및 적용

--- apps/hub/src/app/layout.tsx
+++ apps/hub/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import { GlobalHeader } from '@/components/GlobalHeader';
+import { gamjaFlower } from './fonts';
 
 export const metadata: Metadata = {
     title: 'Create Next App',
@@ -12,7 +13,7 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
     return (
         <html lang="ko">
-            <body className={`antialiased`}>
+            <body className={`antialiased ${gamjaFlower.variable}`}>
                 <GlobalHeader />
                 <main className="max-w-7xl mx-auto mt-10">{children}</main>
             </body>
