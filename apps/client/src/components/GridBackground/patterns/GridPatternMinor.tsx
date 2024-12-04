c59bfb3a8767188ec8fb979245548457b41883f4
p1n9d3v <p1n9@p1n9s-MacBook-Air.local>
2024-12-04 16:53:11 +0900

    🐞 Fix(client): gridPatternMinor transform 속성 제거

--- apps/client/src/components/GridBackground/patterns/GridPatternMinor.tsx
+++ apps/client/src/components/GridBackground/patterns/GridPatternMinor.tsx
@@ -28,7 +28,6 @@ export default ({ points, dimension }: Props) => {
                 width={width}
                 height={height}
                 patternUnits="userSpaceOnUse"
-                patternTransform={`scale(${1 / 5})`}
             >
                 <path
                     d={d1}
