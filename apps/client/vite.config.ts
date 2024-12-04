5981c5d2784dc9ef28896d3dc1d96ae5ec0d82fc
Geonhyuk Seo <sgh4123@naver.com>
2024-12-04 16:07:39 +0900

    🐞 Fix(client): build 에러 수정 및 port를 환경 변수를 통해 받을 수 있도록 설정

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,11 +1,13 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-// https://vite.dev/config/
-export default defineConfig({
-    plugins: [react(), tsconfigPaths()],
-    server: {
-        port: 3001,
-    },
-});
+export default ({ mode }) => {
+    const env = loadEnv(mode, process.cwd());
+    return defineConfig({
+        plugins: [react(), tsconfigPaths()],
+        server: {
+            port: parseInt(env.VITE_PORT as string),
+        },
+    });
+};
