393c434cf1ce2b3ecb9b519b613a1af3be99aea2
p1n9d3v <p1n9@p1n9s-MacBook-Air.local>
2025-01-03 10:51:41 +0900

    feat: Isometric 관련된 util 추가

---
+++ packages/cloud-graph/src/utils/index.ts
@@ -0,0 +1,71 @@
+import { GRID_HEIGHT_3D, GRID_SIZE_2D, GRID_WIDTH_3D } from '@/constants';
+import { Dimension, GridPoint, ScreenPoint } from '@/types';
+
+export const gridToScreen3d = (point: GridPoint) => {
+    const { col, row } = point;
+
+    const x = (col - row) * (GRID_WIDTH_3D / 2);
+    const y = (col + row) * (GRID_HEIGHT_3D / 2);
+
+    return { x, y };
+};
+
+export const screenToGrid3d = (point: ScreenPoint) => {
+    const { x, y } = point;
+
+    const GRID_WIDTH_3D_HALF = GRID_WIDTH_3D / 2;
+    const GRID_HEIGHT_3D_HALF = GRID_HEIGHT_3D / 2;
+    const col = (x / GRID_WIDTH_3D_HALF + y / GRID_HEIGHT_3D_HALF) / 2;
+    const row = (y / GRID_HEIGHT_3D_HALF - x / GRID_WIDTH_3D_HALF) / 2;
+
+    return { col, row };
+};
+
+export const screenToGrid2d = (point: ScreenPoint) => {
+    const { x, y } = point;
+    const col = x / GRID_SIZE_2D;
+    const row = y / GRID_SIZE_2D;
+
+    return { col, row };
+};
+
+export const gridToScreen2d = (point: GridPoint) => {
+    const { col, row } = point;
+    const x = col * GRID_SIZE_2D;
+    const y = row * GRID_SIZE_2D;
+    return { x, y };
+};
+
+export const getSvgPoint = (svg: SVGSVGElement, point: ScreenPoint) => {
+    const { x, y } = point;
+    const svgPoint = svg.createSVGPoint();
+    svgPoint.x = x;
+    svgPoint.y = y;
+    const screenCTM = svg.getScreenCTM();
+    return svgPoint.matrixTransform(screenCTM!.inverse());
+};
+
+export const snapToGrid = (
+    point: ScreenPoint,
+    dimension: Dimension,
+    denominator = 4,
+) => {
+    const cellWidth = dimension === '3d' ? GRID_WIDTH_3D : GRID_SIZE_2D;
+    const cellHeight = dimension === '3d' ? GRID_HEIGHT_3D : GRID_SIZE_2D;
+
+    const snapWidth = cellWidth / denominator;
+    const snapHeight = cellHeight / denominator;
+
+    if (Math.abs(point.x) < snapWidth && Math.abs(point.y) < snapHeight)
+        return null;
+
+    const snappedX = Math.round(point.x / snapWidth) * snapWidth;
+    const snappedY = Math.round(point.y / snapHeight) * snapHeight;
+
+    const screenToGrid = dimension === '3d' ? screenToGrid3d : screenToGrid2d;
+
+    return {
+        screen: { x: snappedX, y: snappedY },
+        grid: screenToGrid({ x: snappedX, y: snappedY }),
+    };
+};
