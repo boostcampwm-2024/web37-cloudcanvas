57deea1351c29404b3e8c79ef9b2117cce9e95ff
p1n9d3v <p1n9@p1n9s-MacBook-Air.local>
2025-01-06 00:33:22 +0900

    refactor: Refactoring Polygon component

--- packages/cloud-graph/src/components/Svg/Polygon.tsx
+++ packages/cloud-graph/src/components/Svg/Polygon.tsx
@@ -1,25 +1,14 @@
 import { ScreenPoint } from '@/types';
 
-function Polygon({
-    points,
-    fill,
-    stroke,
-    strokeWidth = 1,
-}: {
+interface PolygonProps extends Omit<React.ComponentProps<'polygon'>, 'points'> {
     points: ScreenPoint[];
-    fill: string;
-    stroke?: string;
-    strokeWidth?: number;
-}) {
+}
+
+function Polygon(props: PolygonProps) {
+    const { points, ...restProps } = props;
     const pointsStr = points.map((p) => `${p.x},${p.y}`).join(' ');
-    return (
-        <polygon
-            points={pointsStr}
-            fill={fill}
-            stroke={stroke}
-            strokeWidth={strokeWidth}
-        ></polygon>
-    );
+
+    return <polygon points={pointsStr} {...restProps}></polygon>;
 }
 
 export default Polygon;
