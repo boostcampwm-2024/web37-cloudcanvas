161e4a03a1e2a334d5c9562d56b09740add2a82b
Gdm0714 <ehdals45454@naver.com>
2025-01-09 12:36:11 +0900

    💄 Style: 코드 형식 변경

--- packages/validation/schemas/loadBalancer.ts
+++ packages/validation/schemas/loadBalancer.ts
@@ -1,30 +1,46 @@
 import { z } from 'zod';
 import { commonSchema } from './base';
 
-const lbRuleSchema = z.object({
-    protocol_type: z.enum(['HTTP', 'HTTPS', 'TCP', 'SSL']),
-    load_balancer_port: z.number().int().min(1).max(65535),
-    server_port: z.number().int().min(1).max(65535),
-    l7_health_check_path: z.string().optional(),
-    certificate_name: z.string().optional(),
-    proxy_protocol_use_yn: z.enum(['Y', 'N']).optional(),
-}).refine((data) => {
-    if (['HTTP', 'HTTPS'].includes(data.protocol_type) && !data.l7_health_check_path) {
-        return false;
-    }
-    return true;
-}, {
-    message: '17 health check path는 HTTP, HTTPS 프로토콜에서 필수입니다.',
-    path: ['l7_health_check_path'],
-}).refine((data) => {
-    if (['SSL', 'HTTPS'].includes(data.protocol_type) && !data.certificate_name) {
-        return false;
-    }
-    return true;
-}, {
-    message: 'Certificate name은 SSL, HTTPS 프로토콜에서 필수입니다',
-    path: ['certificate_name'],
-});
+const lbRuleSchema = z
+    .object({
+        protocol_type: z.enum(['HTTP', 'HTTPS', 'TCP', 'SSL']),
+        load_balancer_port: z.number().int().min(1).max(65535),
+        server_port: z.number().int().min(1).max(65535),
+        l7_health_check_path: z.string().optional(),
+        certificate_name: z.string().optional(),
+        proxy_protocol_use_yn: z.enum(['Y', 'N']).optional(),
+    })
+    .refine(
+        (data) => {
+            if (
+                ['HTTP', 'HTTPS'].includes(data.protocol_type) &&
+                !data.l7_health_check_path
+            ) {
+                return false;
+            }
+            return true;
+        },
+        {
+            message:
+                '17 health check path는 HTTP, HTTPS 프로토콜에서 필수입니다.',
+            path: ['l7_health_check_path'],
+        },
+    )
+    .refine(
+        (data) => {
+            if (
+                ['SSL', 'HTTPS'].includes(data.protocol_type) &&
+                !data.certificate_name
+            ) {
+                return false;
+            }
+            return true;
+        },
+        {
+            message: 'Certificate name은 SSL, HTTPS 프로토콜에서 필수입니다',
+            path: ['certificate_name'],
+        },
+    );
 
 export const loadBalancerSchema = z.object({
     ...commonSchema,
