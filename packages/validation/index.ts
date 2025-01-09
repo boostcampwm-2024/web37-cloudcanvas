5357c9c35e38557eab9fab50794625138a89b5f0
Gdm0714 <ehdals45454@naver.com>
2025-01-09 12:36:28 +0900

    ✨ Feat: 추가된 속성 검증 추가

--- packages/validation/index.ts
+++ packages/validation/index.ts
@@ -6,15 +6,30 @@ export * from './schemas/vpc';
 export * from './schemas/subnet';
 export * from './schemas/server';
 export * from './schemas/mysql';
+export * from './schemas/redis';
+export * from './schemas/redisConfigGroup';
+export * from './schemas/launchConfiguration';
 
 import { ResourceValidator } from './validator';
 import { vpcSchema } from './schemas/vpc';
 import { subnetSchema } from './schemas/subnet';
 import { serverSchema } from './schemas/server';
+import { redisSchema } from './schemas/redis';
+import { redisConfigGroupSchema } from './schemas/redisConfigGroup';
+import { launchConfigurationSchema } from './schemas/launchConfiguration';
 
 export function initializeValidation(): void {
     ResourceValidator.registerSchema('vpc', vpcSchema);
     ResourceValidator.registerSchema('subnet', subnetSchema);
     ResourceValidator.registerSchema('server', serverSchema);
     ResourceValidator.registerSchema('mysql', mysqlSchema);
+    ResourceValidator.registerSchema('redis', redisSchema);
+    ResourceValidator.registerSchema(
+        'redisconfiggroup',
+        redisConfigGroupSchema,
+    );
+    ResourceValidator.registerSchema(
+        'launchConfiguration',
+        launchConfigurationSchema,
+    );
 }
